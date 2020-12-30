fd6a14a53adac88273bfd8c1253883ffad299d10
Philip Rautiainen <philiprautiainen@outlook.com>
2020-12-30 10:56:58 +0100

    Added highscore page

--- view/__init__.py
+++ view/__init__.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("base.html")
+
+@app.route('/highscore')
+def highscore():
+    return render_template("highscore.html")

1072189d1eb340d3d9e789d453a6c7ac6310cddf
Sayed Jawid Mortazawi <benjimortal@gmail.com>
2021-01-11 13:51:48 +0100

    more routes

--- view/__init__.py
+++ view/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, session
 
 app = Flask(__name__)
 
@@ -7,6 +7,31 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/logga in')
-def inlogning():
+
+
+@app.route('/sign_in/')
+def sign_in():
     return render_template('login.html')
+
+
+@app.route('/sign_in/', methods=["POST"])
+def sign_in_post():
+    return redirect(url_for('profile'))
+
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signup/', methods=["POST"])
+def signup_post():
+    return redirect(url_for('sign_in'))
+
+

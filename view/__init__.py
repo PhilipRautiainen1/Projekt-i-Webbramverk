672f1a3519c497bec6dea6ad630de2f501b321b7
Sara Kaminska <sara.kaminska@hotmail.com>
2021-01-11 13:18:58 +0100

    Add-question page is done

--- view/__init__.py
+++ view/__init__.py
@@ -5,4 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route('/logged-in')
+def logged_in():
+    return render_template('base_user.html')
+
+
+@app.route('/my-page')
+def my_page():
+    return render_template('my_page.html')
+
+
+@app.route('/add-question')
+def add_question():
+    return render_template('add_question.html')
+
+
+@app.route('/not-logged-in')
+def not_logged_in():
+    return render_template('not_logged_in.html')

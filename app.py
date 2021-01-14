4ef5aa24fc386e47d06334adbeeeda0c9175c5aa
Simon Löfgren <simonrlofgren@hotmail.com>
2021-01-14 16:33:21 +0100

    loggin klar

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import bcrypt
+
 from Data_mongo.models import Question
 from Data_mysql.db import engine, Base
-from view import app
+from view import app, add_user
 from Data_mysql.models.users import User
 from Data_mysql.models.friends import Friend
 
@@ -8,18 +10,18 @@ def add_test_question():
     answers = [{
         "answer": "195",
         "correctBool": True
+    },
+        {
+            "answer": "190",
+            "correctBool": False
         },
         {
-        "answer": "190",
-        "correctBool": False
+            "answer": "200",
+            "correctBool": False
         },
         {
-        "answer": "200",
-        "correctBool": False
-        },
-        {
-        "answer": "185",
-        "correctBool": False
+            "answer": "185",
+            "correctBool": False
         }
     ]
 
@@ -28,12 +30,20 @@ def add_test_question():
         "category": "Geografi",
         "diff": 1,
         "answers": answers
-        })
+    })
 
     question.save()
 
+def sql():
+    Base.metadata.create_all(engine)
+    email='simon@email.com'
+    username='simon'
+    password='s3cr37'
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(str.encode(password), salt)
+    add_user(email, username, hashed_password)
+
 #add_test_question()
-#Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
    app.run(debug=True)

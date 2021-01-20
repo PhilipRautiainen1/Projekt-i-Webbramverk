207679e4e22d3972b12dcc7cb82a64856657e467
Sara Kaminska <sara.kaminska@hotmail.com>
2021-01-20 13:52:40 +0100

    New function adding questions from API

--- Data_mongo/repositories/question_repository.py
+++ Data_mongo/repositories/question_repository.py
@@ -1,4 +1,8 @@
 from Data_mongo.models import Question
+from view.tools import unescape_dict
+import json
+import requests
+import html
 
 
 def add_question(question):
@@ -18,3 +22,35 @@ def add_question(question):
              'correctBool': False}]
     })
     question.save()
+
+
+def add_questions():
+    url = 'https://opentdb.com/api.php?amount=50&type=multiple'
+    data = requests.get(url)
+    if data.status_code == 200:
+        json_data = json.loads(data.text)
+        questions = json_data['results']
+
+        for q in questions:
+            q = unescape_dict(q)
+            q['incorrect_answers'] = [html.unescape(answer) for answer in q['incorrect_answers']]
+            question = Question({
+                'question': q['question'],
+                'category': q['category'],
+                'diff': q['difficulty'],
+                'answers': [
+                    {'answer': q['correct_answer'],
+                     'correctBool': True},
+                    {'answer': q['incorrect_answers'][0],
+                     'correctBool': False},
+                    {'answer': q['incorrect_answers'][1],
+                     'correctBool': False},
+                    {'answer': q['incorrect_answers'][2],
+                     'correctBool': False}
+                ]})
+            question.save()
+
+    else:
+        print('Could not reach the API')
+
+#add_questions()

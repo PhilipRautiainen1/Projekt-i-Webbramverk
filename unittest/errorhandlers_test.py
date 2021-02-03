7c7e17947e10a4c1984940c70df631f4ef560530
Sara Kaminska <sara.kaminska@hotmail.com>
2021-02-03 15:35:23 +0100

    Errorhandlers test

---
+++ unittest/errorhandlers_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from selenium import webdriver
+
+
+class LogInTests(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome('chromedriver.exe')
+
+    def test_404(self):
+        self.driver.get('http://127.0.0.1:5000/min-sida')
+        self.assertIn('ERROR', self.driver.page_source)
+
+    def tearDown(self):
+        self.driver.close()

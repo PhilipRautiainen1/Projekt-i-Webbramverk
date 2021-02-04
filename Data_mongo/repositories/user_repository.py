20001d75c0ca53569c0c88bfca9956cb734ce510
Sara Kaminska <sara.kaminska@hotmail.com>
2021-02-04 14:19:40 +0100

    Added remove_friend function to repository

--- Data_mongo/repositories/user_repository.py
+++ Data_mongo/repositories/user_repository.py
@@ -1,11 +1,14 @@
 from Data_mongo.models import User
+from bson.objectid import ObjectId
 
 
 def get_user(username):
     return User.find(username=username).first_or_none()
 
+
 def get_user_by_id(_id):
     return User.find(_id=_id).first_or_none()
+
 
 def add_user(email, username, hashed_password):
     user = User({
@@ -17,14 +20,20 @@ def add_user(email, username, hashed_password):
         })
     user.save()
 
+
 def add_friend(user, friend):
     friends = user.friends
     friends.append(friend._id)
     user.collection.update({"_id": user._id}, {"$set": {'friends': friends}}, upsert=True)
 
 
+def remove_friend(user, friend_id):
+    user.collection.update_one({'_id': user._id}, {'$pull': {'friends': ObjectId(friend_id)}})
+
+
 def get_all_users():
     return User.all()
+
 
 def save_score(score, user):
     new_score = score + user.score

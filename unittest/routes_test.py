b0a93c48880e95af6f6aced0229d41ebd3e5d98d
Philip Rautiainen <philiprautiainen@outlook.com>
2021-02-04 01:24:31 +0100

    Finished Log Out test

--- unittest/routes_test.py
+++ unittest/routes_test.py
@@ -1,10 +1,37 @@
-import unittest
+from idlelib import browser
+from telnetlib import EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from unittest import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class RoutesTestCase(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome('chromedriver.exe')
+        self.driver.get('http://127.0.0.1:5000/sign_in')
+        self.username = self.driver.find_element_by_id('l_username')
+        self.password = self.driver.find_element_by_id('l_password')
+        self.submit = self.driver.find_element_by_id('l_submit')
+
+    def testLogOut(self):
+        switch = False
+        self.username.send_keys('TestUser')
+        self.password.send_keys('superhemligt')
+        self.submit.send_keys(Keys.RETURN)
+        self.driver.implicitly_wait(3)
+        self.assertIn('Min sida', self.driver.page_source)
+        self.log_out = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'sign-out')))
+        self.log_out.click()
+        if WebDriverWait(self.driver, 20).until(EC.url_changes):
+            switch = True
+        self.assertIs(switch, True)
+
+    def tearDown(self):
+        self.driver.close()
 
 
-if __name__ == '__main__':
-    unittest.main()
+

8bdb5bbf0257b3da788354144a05b3ff46a3a60f
Philip Rautiainen <philiprautiainen@outlook.com>
2021-02-05 15:41:05 +0100

    Timer animation finished

--- view/static/js/timer.js
+++ view/static/js/timer.js
@@ -5,4 +5,25 @@ function changeButtons(){
 
 function Timer(){
     setTimeout(changeButtons, 15000)
+    var i = 0;
+    if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("bar");
+    var width = 1;
+    var id = setInterval(frame, 150);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
 }
+
+
+
+
+

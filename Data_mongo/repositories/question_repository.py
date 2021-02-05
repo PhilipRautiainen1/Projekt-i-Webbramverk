8afc2cd9410849b3ce751c2bcd392078f5a8f168
Sara Kaminska <sara.kaminska@hotmail.com>
2021-02-05 11:38:51 +0100

    Bugfix for question_repository

--- Data_mongo/repositories/question_repository.py
+++ Data_mongo/repositories/question_repository.py
@@ -55,6 +55,10 @@ def add_questions():
         print('Could not reach the API')
 
 
+def get_all_questions():
+    return Question.all()
+
+
 def get_questions(category, no):
     quest = []
     cat_quest = []
@@ -81,6 +85,3 @@ def get_questions(category, no):
                 quest.append(cat_quest[i])
 
     return [q.to_dict() for q in quest]
-
-
-

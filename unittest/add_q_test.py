192b41f79e295355ff3e3e50b71118ced4622799
Sayed Jawid Mortazawi <benjimortal@gmail.com>
2021-02-08 16:13:16 +0100

    added add_q_test.py

--- unittest/add_q_test.py
+++ unittest/add_q_test.py
@@ -2,10 +2,6 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 
@@ -22,22 +18,66 @@ class AddQuestionsTests(unittest.TestCase):
         self.submit.send_keys(Keys.RETURN)
         self.driver.get('http://127.0.0.1:5000/add-question')
 
+        self.question= self.driver.find_element_by_id('l_question')
+        self.right_answer = self.driver.find_element_by_id('l_right_answer')
+        self.wrong_answer1 = self.driver.find_element_by_id('l_wrong_answer1')
+        self.wrong_answer2 = self.driver.find_element_by_id('l_wrong_answer2')
+        self.wrong_answer3 = self.driver.find_element_by_id('l_wrong_answer3')
+        self.submit =self.driver.find_element_by_id('submit')
+
+
 
     def test_add_q(self):
         self.driver.find_element_by_class_name('category-select').click()
         select_cat = Select(self.driver.find_element_by_id('category'))
-        select_cat.select_by_value('Sports')
-        # select_asn = Select(self.driver.find_element_by_name('number'))
-        # select_asn = Select(self.driver.find_elements_by_name('question'))
-        # select_asn.deselect_all()
-        # self.driver.find_element_by_id('submit').click()
-        # self.assertIn('Lägg till', self.driver.page_source)
-        time.sleep(5)
+        select_cat.select_by_value('General Knowledge')
+
+        self.question.send_keys('What is the distance from the Earth to the sun?')
+        self.right_answer.send_keys('150 million kilometers')
+        self.wrong_answer1.send_keys('100 million kilometers')
+        self.wrong_answer2.send_keys('200 million kilometers')
+        self.wrong_answer3.send_keys('300 million kilometers')
+        self.submit.send_keys(Keys.RETURN)
+        self.driver.implicitly_wait(3)
+        self.assertIn('Frågan har blivit tillagd!', self.driver.page_source)
+
+
+
+    def test_duplicates_add_q(self):
+        self.driver.find_element_by_class_name('category-select').click()
+        select_cat = Select(self.driver.find_element_by_id('category'))
+        select_cat.select_by_value('General Knowledge')
+
+        self.question.send_keys('What is the distance from the Earth to the sun?')
+        self.right_answer.send_keys('150 million kilometers')
+        self.wrong_answer1.send_keys('100 million kilometers')
+        self.wrong_answer2.send_keys('200 million kilometers')
+        self.wrong_answer3.send_keys('300 million kilometers')
+        self.submit.send_keys(Keys.RETURN)
+        self.driver.implicitly_wait(3)
+        self.assertIn('Din fråga är för lik en som redan existerar!', self.driver.page_source)
+
+
+
+    def test_double_add_answers(self):
+        self.driver.find_element_by_class_name('category-select').click()
+        select_cat = Select(self.driver.find_element_by_id('category'))
+        select_cat.select_by_value('General Knowledge')
+
+        self.question.send_keys('Where is Lionel Messi from?')
+        self.right_answer.send_keys('Rosario, Argentina')
+        self.wrong_answer1.send_keys('Funchal, Portugal')
+        self.wrong_answer2.send_keys('Mogi das Cruzes, State of São Paulo, Brazil')
+        self.wrong_answer3.send_keys('Funchal, Portugal')
+        self.submit.send_keys(Keys.RETURN)
+        self.driver.implicitly_wait(3)
+        self.assertIn('Felaktiga svar måste vara unika!', self.driver.page_source)
 
 
 
     def tearDown(self):
         self.driver.close()
+
 
 if __name__ == '__main__':
     unittest.main()
